e24c7eac4e4b10f109c4d578e8d208815e6957ba
ijayiforieveri <43195216+ijayiforieveri@users.noreply.github.com>
2019-02-15 19:21:36 -0800

    Add files via upload

--- ee250/lab04/rpi_pub_and_sub.py
+++ ee250/lab04/rpi_pub_and_sub.py
@@ -4,17 +4,61 @@ Run rpi_pub_and_sub.py on your Raspberry Pi."""
 
 import paho.mqtt.client as mqtt
 import time
+import sys
+from grove_rgb_lcd import *
+sys.path.append('../../Software/Python/grove_rgb_lcd')
+import grovepi
+dht_sensor_port = 7
+
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
+    client.subscribe("rpi-jaeishin/led")
+    client.message_callback_add("rpi-jaeishin/led", ledcustom)
+    #see line 29 for ledcustom functions
+     
+    client.subscribe("rpi-jaeishin/lcd")
+    client.message_callback_add("rpi-jaeishin/lcd", lcdcustoms)
+    #see line 37 for lcdcustom functions
 
     #subscribe to topics of interest here
 
-#Default message callback. Please use custom callbacks.
+
+#function of led customs for subscribing
+def ledcustom(client, userdata, message):
+    if str(message.payload, "utf-8") == "LED_ON":
+       grovepi.digitalWrite(led, 1)
+      
+    else: 
+       grovepi.digitalWrite(led, 0)
+
+#function of lcd customs for subscribing
+def lcdcustoms(client, userdata, message):
+    if str(message.payload, "utf-8") == "w":
+        setText_norefresh("w")
+        
+    elif str(message.payload, "utf-8") == "a":  
+        setText_norefresh("a")
+       
+    elif str(message.payload, "utf-8") == "s":
+        setText_norefresh("s")
+        
+    elif str(message.payload, "utf-8") == "d":  
+        setText_norefresh("d")
+
+    
 def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
+    print("on_message: msg.payload is of type " + str(type(msg.payload)))
+
+ #Default message callback. Please use custom callbacks.
 
 if __name__ == '__main__':
+
+    PORT = 4
+    led = 5
+    button = 3
     #this section is covered in publisher_and_subscriber_example.py
     client = mqtt.Client()
     client.on_message = on_message
@@ -23,7 +67,31 @@ if __name__ == '__main__':
     client.loop_start()
 
     while True:
-        print("delete this line")
+        #flashes LCD screen to green
+        setRGB(0,128,64)
+        setRGB(0,255,0)
+        ultrasonic = grovepi.ultrasonicRead(PORT)
+        button_status = grovepi.digitalRead(button)
+        #publish the ultrasonic range
+        client.publish("rpi-jaeishin/ultrasonic", str(ultrasonic))
+        print(ultrasonic)
         time.sleep(1)
+
+        #when button is pressed publish button pressed
+    if button_status:
+        print("button pressed")
+        client.publish("rpi-jaeishin/button", "Button pressed")
+
+
+
+   
+    
+       
+        
+       
+
+       
+
+
             
 
